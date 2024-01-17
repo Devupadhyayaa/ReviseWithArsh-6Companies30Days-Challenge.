b3c933d62749e2ea6b2b14b2cef5b3094ca855f8
Devansh Upadhyay <92574975+Devupadhyayaa@users.noreply.github.com>
2024-01-17 16:08:53 +0530

    Trim a Binary Search Tree.cpp

---
+++ Trim a Binary Search Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root==NULL)return NULL;
+        if(root->val>high)return trimBST(root->left,low,high);
+        if(root->val<low)return trimBST(root->right,low,high);
+        else{
+            root->left=trimBST(root->left,low,high);
+            root->right=trimBST(root->right,low,high);
+            return root;
+        }
+    }
+};
